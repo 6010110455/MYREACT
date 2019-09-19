3c8a4311d83dc6a9fed76fbb6ce404206f00ebc7
6010110455 <6010110455@psu.ac.th>
2019-09-19 11:32:15 +0700

    first commit

---
+++ card-game/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import WordCard from './WordCard';
+    class App extends Component {
+        render() {  
+            return (
+                <div >
+                 <WordCard value="BENZ"/>
+                </div>
+            );
+        }
+    }
+export default App;
